e4aa5d3e3f36d2b4ea0c681197f4579bf9ac0c06
Bogatyrenko Vladimir <uamvua@mail.ru>
2019-03-30 01:41:25 +0500

    Drag and Drop

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,22 +1,27 @@
 import render from '../templates/friends.hbs'
+   
+    let popupClose = document.querySelector('.popup__close');
+    let popup = document.querySelector('.popup');    
 
-function popupOpen(coord) {
-
-   let popupReviewsList = document.querySelector('.popup__list');
-   let userName = document.querySelector('.user-name');
-   let place = document.querySelector('.place');
-   let reviews = document.querySelector('.reviews-text');
-   let popupClose = document.querySelector('.popup__close')
-   let addButton = document.querySelector('.button__add'); 
-   let popupForm = document.querySelector('.popup');
-   let data = {};
-   let mas = [];
-   let masResult;   
+function popupOpen(obj, myMap, coordMouse, clusterer, hintContent) {
+    let popupReviewsList = document.querySelector('.popup__list');    
+    
+    let [posX, posY] = [...coordMouse];
+      console.log(posX);
+      console.log(posY);
   
-   popupClose.addEventListener('click', ()=> {
-    popupForm.style.display='none';    
+     popup.style.display='block';
+     popup.style.top = `${posY}px`;
+     popup.style.left = `${posX}px`;      
+       
+    popupClose.addEventListener('click', ()=> {
+ popup.style.display='none';    
    });
 
+   dragAnddrop()
+
+  
+/*
     function masRev() {        
     data.userName = userName.value;
     data.place = place.value;
@@ -32,18 +37,139 @@ function popupOpen(coord) {
         e.preventDefault();
         masResult = masRev();
         console.log(masResult);
-        popupReviewsList.innerHTML = render({apidata: masResult}); 
-        userName.value = '';
-        place.value = '';
-        reviews.value = '';       
+        popupReviewsList.innerHTML = render({apidata: masResult});        
     });
 
-   if (!reviews.length) {
-        popupReviewsList.textContent = 'Нет отзывов...'
-   }   
+    //createPlacemark(obj, myMap, coordMouse, clusterer, popupReviewsList);
+*/
+    
+   /*
+    userName.value = '';
+    place.value = '';
+    reviews.value = '';
+*/
+    popupReviewsList
+
+    addFeedback(obj, myMap, coordMouse, clusterer, popupReviewsList, hintContent);
+    
 }
 
 
+function addFeedback(obj, myMap, coordMouse, clusterer, popupReviewsList, hintContent) {
+    let data = {};
+    let mas = [];
+    let userName = document.querySelector('.user-name');
+    let addButton = document.querySelector('.button__add');
+    let place = document.querySelector('.place');
+    let reviews = document.querySelector('.reviews-text');    
+   
+    popupReviewsList.innerHTML = hintContent;
+    console.log('HINT ' + hintContent);
+/*
+    if (!reviews.length) {
+        popupReviewsList.textContent = 'Нет отзывов...'
+    }
+    */
+    addButton.addEventListener('click', (e)=> {       
+        e.preventDefault();
+        if ((userName.value && place.value && reviews.value) === '') {
+            alert('Заполните все поля');
+            return;
+        };
+        data = {};
+        console.log('клик');
+        data.coords = obj.coords,
+        data.adress = obj.adress,      
+        data.userName = userName.value;
+        data.place = place.value;
+        data.reviews = reviews.value;       
+        data.dataRev = new Date().toLocaleDateString();
+        
+        //mas.push(data);
+        obj.comments.push(data);
+        console.log(obj.comments);
+        let arr =  obj.comments;
+        popupReviewsList.innerHTML = render({arr});
+        //console.log( 'popupReviewsList.innerHTML ' + popupReviewsList.innerHTML);
+        /*
+        userName.value = '';
+        place.value = '';
+        reviews.value = '';
+        */      
+        createPlacemark(obj, myMap, coordMouse, clusterer, popupReviewsList, data);       
+    });       
+}
+
+function createPlacemark(obj, myMap, coordMouse, clusterer, popupReviewsList, data) {
+    let popupItemPlace = document.querySelector('.popup__item-place');
+    var myPlacemark = new ymaps.Placemark(obj.coords, {
+        hintContent: popupReviewsList.children[0].innerHTML,
+        //popupoonContent: obj.adress + popupReviewsList.children[0].innerHTML,
+        baloonContentName: data.userName,
+        baloonContentAdress: data.adress,
+        popupoonContentBody: data.reviews,
+        popupoonContentHeader: data.place,
+        popupoonContentFooter: data.dataRev
+
+    }, {
+        preset: 'islands#violetDotIcon',
+        openHintOnHover: false,
+        draggable: false
+    });    
+    console.log(myPlacemark.properties);
+    myMap.geoObjects.add(myPlacemark);
+    clusterer.add(myPlacemark);   
+    
+    myPlacemark.events.add('click', () => {
+        popupOpen(obj, myMap, coordMouse, clusterer, myPlacemark.properties._data.hintContent);
+        });
+    }
+
+    function dragAnddrop() {
+        let popupHeader = document.querySelector('.popup__header');
+        popupHeader.onmousedown = function(e) {
+
+            var coords = getCoords(popup);
+            var shiftX = e.pageX - coords.left;
+            var shiftY = e.pageY - coords.top;
+          
+            popup.style.position = 'absolute';
+            document.body.appendChild(popup);
+            moveAt(e);
+          
+            popup.style.zIndex = 1000; // над другими элементами
+          
+            function moveAt(e) {
+              popup.style.left = e.pageX - shiftX + 'px';
+              popup.style.top = e.pageY - shiftY + 'px';
+            }
+          
+            popupHeader.onmousemove = function(e) {
+              moveAt(e);
+            };
+          
+            popupHeader.onmouseup = function() {
+                popupHeader.onmousemove = null;
+                popupHeader.onmouseup = null;
+            };
+          
+          }
+          
+          popup.ondragstart = function() {
+            return false;
+          };
+          
+          function getCoords(elem) {   // кроме IE8-
+            var box = elem.getBoundingClientRect();
+            return {
+              top: box.top + pageYOffset,
+              left: box.left + pageXOffset
+            };
+          }
+    }
+
+    
+
 export {
-    popupOpen      
+    popupOpen
 }
--- src/js/ymaps.js
+++ src/js/ymaps.js
@@ -1,18 +1,50 @@
 import { popupOpen } from './popup'
 
 let popupForm = document.querySelector('.popup');
+//let popupReviewsList = document.querySelector('.popup__list');
+
 function initMap() {
   ymaps.ready(() => {
-      var myPlacemark;
+      //var myPlacemark;
       var myMap = new ymaps.Map("map", {
       center: [55.76, 37.64],
       zoom: 7
     });
 
+    var customItemContentLayout = ymaps.templateLayoutFactory.createClass(
+      // Флаг "raw" означает, что данные вставляют "как есть" без экранирования html.
+      
+      '<h2 class=ballon_header>{{ properties.balloonContentHeader|raw }}</h2>' +
+          '< class=ballon_name>{{ properties.baloonContentName|raw }}</>' +
+          '< class=ballon_adress>{{ properties.baloonContentAdress|raw }}</>' +
+          '<div class=ballon_body>{{ properties.balloonContentBody|raw }}</div>' +
+          '<div class=ballon_footer>{{ properties.balloonContentFooter|raw }}</div>'
+    );
+    
+    var clusterer = new ymaps.Clusterer({
+      clusterDisableClickZoom: true,
+      // Используем макет "карусель"
+      clusterBalloonContentLayout: "cluster#balloonCarousel",
+      // Запрещаем зацикливание списка при постраничной навигации.
+      clusterBalloonCycling: false,
+      // Настройка внешнего вида панели навигации.
+      // Элементами панели навигации будут маркеры.
+      //clusterBalloonPagerType: "marker",
+      // Количество элементов в панели.
+      clusterBalloonPagerSize: 6
+    });
+
+    myMap.geoObjects.add(clusterer);
+    
     myMap.events.add('click', function (e) {
       var coords = e.get('coords');
       var coordMouse = e.get('position');
-
+      console.log(coords, coordMouse);      
+      
+      //popupOpen(myMap, coordMouse, clusterer, coords);
+      getAddress(myMap, coordMouse, clusterer, coords);
+      //popupOpen(obj, myMap, doordMouse, clusterer, )
+/*
       detectCoord();
 
       function detectCoord() {
@@ -24,14 +56,17 @@ function initMap() {
         popupForm.style.top = `${posY}px`;
         popupForm.style.left = `${posX}px`;
 
-        popupOpen(coords);
-        getAddress(coords);
-      }
-      
+         getAddress(coords);
+        //popupOpen(coords);
+             
 
+    } */
+/*
       // Если нет – создаем.      
         myPlacemark = createPlacemark(coords);
         myMap.geoObjects.add(myPlacemark);
+        clusterer.add(myPlacemark);
+
         // Слушаем событие окончания перетаскивания на метке.
         myPlacemark.events.add('dragend', function () {
             getAddress(myPlacemark.geometry.getCoordinates());
@@ -44,30 +79,31 @@ function initMap() {
           coordMouse = e.get('position');
           detectCoord()
         });
-
-      });
-     
+*/
+      });     
     }); 
      
   }
-  
+  /*
   // Создание метки.
     function createPlacemark(coords) {
       return new ymaps.Placemark(coords,  {
-          //hintContent: popup.children[номер].innerHTML,
-         // balloonContent: obj.address + popup.children[номер].innerHTML
+          //hintContent: popupReviewsList.children[3].innerHTML,
+          //balloonContent: obj.address + popupReviewsList.children[3].innerHTML
         }, {
           preset: 'islands#violetDotIcon',
           openHintOnHover: false,
           draggable: false
       })
     }
-
+*/
   // Определяем адрес по координатам (обратное геокодирование).
-    function getAddress(coords) {
+    function getAddress(myMap, coordMouse, clusterer, coords) {
       ymaps.geocode(coords).then(function (res) {
           var firstGeoObject = res.geoObjects.get(0);
-          let adress = document.querySelector('.popup__header-adress');        
+          let adress = document.querySelector('.popup__header-adress');
+          let obj = {};
+          /*
           // Формируем строку с данными об объекте.
           [
             // Название населенного пункта или вышестоящее административно-территориальное образование.
@@ -75,10 +111,21 @@ function initMap() {
             //Получаем путь до топонима, если метод вернул null, запрашиваем наименование здания.
             firstGeoObject.getThoroughfare() || firstGeoObject.getPremise()
           ].filter(Boolean).join(', '),          
-          console.log(firstGeoObject.getAddressLine());
-          adress.textContent = firstGeoObject.getAddressLine();
+          //console.log(firstGeoObject.getAddressLine());
+          //adress.textContent = firstGeoObject.getAddressLine(); */
+          obj.coords = coords;
+          obj.adress = firstGeoObject.getAddressLine();
+          obj.comments = [];
+          adress.textContent = obj.adress;
+          console.log('obj.coords ' + obj.coords);
+          popupOpen(obj, myMap, coordMouse, clusterer, ' ');
       });
+      
     }
+
+    function detectCoord(coord, coordMouse) {
+
+    };
  
 export {
   initMap
